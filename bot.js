f316c453be18f34adc32bbc2b1482bc12ca9cee3
ampersky86 <gasttello@gmail.com>
2018-03-15 00:16:38 +0300

    Был удален токен из кода, теперь он берется из файла

--- bot.js
+++ bot.js
@@ -4,19 +4,22 @@ const TelegramBot = require('node-telegram-bot-api');
 const subprocess =require('child_process').exec;
 const request = require('request');
 const q=require('q');
+var fs = require("fs");
 
-var token = '379599074:AAEM5tTL5pUXkiEk-cBcTPv_XFLOaNJiBCY';
+
+var token = fs.readFileSync("token.txt", "utf8");
 var bot = new TelegramBot(token, {polling: true});
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     var fromId = msg.from.id;
     var name = msg.from.first_name;
-    if (msg.text =='/status') { runCommand().then(function (data) {
+    if (msg.text =='/ls') { runCommand().then(function (data) {
        bot.sendMessage(fromId, data); 
    }, function (error) {
       bot.sendMessage(fromId, error); 
    });
   }   
+  
     else{
     bot.sendMessage(chatId, "Добрый вечер,"+ name + "!", {caption: "I'm a bot!"});
     bot.sendMessage(chatId, "Твой никнейм "+msg.from.username);
@@ -46,7 +49,7 @@ function runCommand() {
 }
 
 function Execute(callback) {
- var exec = subprocess('node ./script.js', callback);
+ var exec = subprocess('ls', callback);
  /*ls.stdout.on('data', callback );*/
  
 }
